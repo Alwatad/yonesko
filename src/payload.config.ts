c20f9d16c036e35c0cf3710af243a8d48e662136
Abdullah <121164316+3km-0@users.noreply.github.com>
2025-08-06 15:21:11 +0300

    fix: 33

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -120,7 +120,7 @@ export default buildConfig({
       },
       options: "--client-encoding=UTF8",
     },
-    push: process.env.NODE_ENV === "development",
+    //push: process.env.NODE_ENV === "development",
     migrationDir: path.resolve(dirname, "migrations"),
   }),
   collections: [
