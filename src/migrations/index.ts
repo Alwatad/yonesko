9b15da6b9ace45fc91eb21765142d4b50e69bccf
Abdullah <121164316+3km-0@users.noreply.github.com>
2025-08-07 22:56:12 +0300

    fix: Remove incomplete GCC countries migration causing enum errors
    - Deleted 20250807_182712 migration that only added 'sa' and 'ae'
    - This was causing 22P02 enum errors when seeder tried to use 'kw', 'qa', 'bh', 'om'
    - Updated migration index to remove deleted migration reference
    - Build now works properly without enum conflicts

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,4 @@
 import * as migration_20250807_141143_initial_schema from './20250807_141143_initial_schema';
-import * as migration_20250807_182712 from './20250807_182712';
 import * as migration_20250807_185000_add_cash_payment from './20250807_185000_add_cash_payment';
 
 export const migrations = [
@@ -7,11 +6,6 @@ export const migrations = [
     up: migration_20250807_141143_initial_schema.up,
     down: migration_20250807_141143_initial_schema.down,
     name: '20250807_141143_initial_schema',
-  },
-  {
-    up: migration_20250807_182712.up,
-    down: migration_20250807_182712.down,
-    name: '20250807_182712',
   },
   {
     up: migration_20250807_185000_add_cash_payment.up,
